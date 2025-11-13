47dda2c95cb4233ba8d37cd283126d168d9e3860
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-11-13 23:37:24 +0530

    Inner Joins

--- joins/inner_joins_2.sql
+++ joins/inner_joins_2.sql
@@ -4,3 +4,9 @@ use practice;
 
 # List all students who are not enrolled in any course
 select s.name from students as s left join enrollments as e on s.student_id = e.student_id where e.course_id is null ;
+
+# Show all courses that currently have no students enrolled
+select c.course_name from courses as c left join enrollments as e on c.course_id = e.course_id where e.student_id is NULL ;
+
+# Display each department and the number of distinct courses its students are enrolled in
+select s.department, count(distinct c.course_name) as total_course from students as s join enrollments as e on s.student_id = e.student_id join courses as c on c.course_id = e.course_id group by s.department ;
