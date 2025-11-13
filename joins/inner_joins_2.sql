08d8f3d217c08d39e45baa12e53c53840c9f8670
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-11-13 23:36:14 +0530

    Inner Joins

--- joins/inner_joins_2.sql
+++ joins/inner_joins_2.sql
@@ -3,4 +3,4 @@
 use practice;
 
 # List all students who are not enrolled in any course
-select s.name from students as s left join enrollments as e on s.student_id = e.student_id where e.course_id = null ;
+select s.name from students as s left join enrollments as e on s.student_id = e.student_id where e.course_id is null ;
