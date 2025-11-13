dc64ad2284a4d3e0b2594215f17d5990a098f007
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-11-13 23:37:55 +0530

    Inner Joins

--- joins/inner_joins_2.sql
+++ joins/inner_joins_2.sql
@@ -10,3 +10,6 @@ select c.course_name from courses as c left join enrollments as e on c.course_id
 
 # Display each department and the number of distinct courses its students are enrolled in
 select s.department, count(distinct c.course_name) as total_course from students as s join enrollments as e on s.student_id = e.student_id join courses as c on c.course_id = e.course_id group by s.department ;
+
+# List tutors who teach more than one courses
+select c.tutor, count(distinct c.course_id) as course_number from courses as c join enrollments as e on c.course_id = e.course_id group by c.tutor having count(c.course_id) > 1 ;
