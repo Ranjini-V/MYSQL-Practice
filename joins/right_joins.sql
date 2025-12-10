dbd9ccb5613e99e34823d556dccce9422a1f93cb
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-12-10 22:54:06 +0530

    Right Joins

--- joins/right_joins.sql
+++ joins/right_joins.sql
@@ -5,3 +5,6 @@ use practice ;
 create table customers ( customer_id int primary key, customer_name varchar(20));
 create table products ( product_id int primary key, product_name varchar(20));
 create table orders (order_id int primary key, customer_id int, product_id int);
+
+insert into customers values (1, 'Alice'), (2, 'Bob'), (3, 'Charlie'), (4, 'David');
+
